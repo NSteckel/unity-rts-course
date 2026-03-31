d0bb4ddcbe1719a279d45e270fb67af13c8e0b58
Nate Steckel <nate.steckel@gmail.com>
2026-03-31 03:57:13 -0400

    Preliminary keyboard camera movement

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInput : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+namespace Nate.RTS
+{ 
+
+    public class PlayerInput : MonoBehaviour
     {
-        
+        // move this transform based on keyboard input from player
+        [SerializeField] private Transform cameraTarget;
+        [SerializeField] private float keyboardPanSpeed = 5f;
+
+
+        void Update()
+        {
+            // moveAmount is how much we want the keyboard to be moved by as a vector 
+            // we only use the x and the z of the vector because we don't want it to move on the y axis (higher / lower)
+            
+            // camera movement based on keyboard input
+            Vector2 moveAmount = Vector2.zero;
+
+            if (Keyboard.current.upArrowKey.isPressed)
+            {
+                moveAmount.y += keyboardPanSpeed;
+            }
+            if (Keyboard.current.leftArrowKey.isPressed)
+            {
+                moveAmount.x -= keyboardPanSpeed;
+            }
+            if (Keyboard.current.downArrowKey.isPressed)
+            {
+                moveAmount.y -= keyboardPanSpeed;
+            }
+            if (Keyboard.current.rightArrowKey.isPressed)
+            {
+                moveAmount.x += keyboardPanSpeed;
+            }
+
+            moveAmount *= Time.deltaTime;
+            cameraTarget.position += new Vector3(moveAmount.x, 0, moveAmount.y);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
